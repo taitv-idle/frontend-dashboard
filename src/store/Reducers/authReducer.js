1dc637e24b87ad7f5ffb0ace9d633a760315bb4c
Devtaitv <taikaka2015@gmail.com>
2024-12-25 17:49:29 +0700

    Complete admin login function

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,16 +1,19 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import axios from "axios";
+import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
     async(info) =>{
         try {
-            console.log(info);
-            // const {data} = await axios.post('/admin-login', info, {withCredentials: true});
+            // console.log(info);
+            const {data} = await api.post('/admin-login', info, {withCredentials: true});
             // console.log(data);
+            if(data.message === 'Login success' && data.token !== null){
+                console.log('Dang nhap thanh cong');
+            }
 
         }catch (e) {
-            console.log(e);
+            console.log(e.response.data);
         }
     }
 
