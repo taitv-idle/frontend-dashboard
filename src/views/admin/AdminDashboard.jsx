43d39ceb734b7ac2ec88fafa2a09e2dd2fafd701
Devtaitv <taikaka2015@gmail.com>
2025-03-23 17:16:34 +0700

    thiet ke don hang gan day| dashboard

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -83,7 +83,7 @@ const AdminDashboard = () => {
 
                 <div className='flex justify-between items-center p-5 bg-[#3de0c7] rounded-md gap-3'>
                     <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
-                        <h2 className='text-3xl font-bold'>$2025</h2>
+                        <h2 className='text-3xl font-bold'>đ2025</h2>
                         <span className='text-md font-medium'>Doanh thu</span>
                     </div>
                     <div className='w-[40px] h-[47px] rounded-full bg-red-400 flex justify-center items-center text-xl'>
@@ -113,14 +113,13 @@ const AdminDashboard = () => {
 
                 <div className='flex justify-between items-center p-5 bg-[#dcc029] rounded-md gap-3'>
                     <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
-                        <h2 className='text-3xl font-bold'>$2025</h2>
-                        <span className='text-md font-medium'>Orders</span>
+                        <h2 className='text-3xl font-bold'>đ2025</h2>
+                        <span className='text-md font-medium'>Đơn hàng</span>
                     </div>
                     <div className='w-[40px] h-[47px] rounded-full bg-[#43dc27] flex justify-center items-center text-xl'>
                         <BiCartAdd className='text-[#fae8e8] shadow-lg'/>
                     </div>
                 </div>
-
             </div>
 
             <div className='w-full flex flex-wrap mt-7'>
@@ -186,6 +185,36 @@ const AdminDashboard = () => {
                             </li>
                         </ol>
                     </div>
+                </div>
+            </div>
+
+            <div className='w-full p-4 bg-[#df5c3f] rounded-md mt-6'>
+                <div className='flex justify-between items-center'>
+                    <h2 className='font-semibold text-lg text-[#d0d2d6] pb-3'>Đơn hàng gần đây</h2>
+                    <Link className='font-semibold text-sm text-[#d0d2d6]' to="#">Xem tất cả</Link>
+                </div>
+
+                <div className="relative overflow-x-auto">
+                    <table className='w-full text-sm text-left text-[#d0d2d6] uppercase border-b border-slate-600'>
+                        <thead className='text-sm text-[#d0d2d6] uppercase border-b border-slate-700'>
+                        <tr>
+                            <th scope='col' className='py-3 px-4'>ID đơn hàng</th>
+                            <th scope='col' className='py-3 px-4'>Giá</th>
+                            <th scope='col' className='py-3 px-4'>Trạng thái thanh toán</th>
+                            <th scope='col' className='py-3 px-4'>Trạng thái đặt hàng</th>
+                            <th scope='col' className='py-3 px-4'>Hoạt động</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td className='py-3 px-6 font-medium whitespace-nowrap'>#1212</td>
+                            <td className='py-3 px-6 font-medium whitespace-nowrap'>đ456000</td>
+                            <td className='py-3 px-6 font-medium whitespace-nowrap'>Đang chờ</td>
+                            <td className='py-3 px-6 font-medium whitespace-nowrap'>Đang chờ</td>
+                            <td className='py-3 px-6 font-medium whitespace-nowrap'>Xem</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
